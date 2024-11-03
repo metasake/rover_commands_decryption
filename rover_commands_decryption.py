c09fc845713346d4e4d6fe80408f19957be59bbc
metasake <metabake@yandex.ru>
2024-11-03 15:38:12 +0300

    Rover Commands Decryption v1.2
    Formatting and bug fix following comments from reviewer.

--- rover_commands_decryption.py
+++ rover_commands_decryption.py
@@ -3,7 +3,7 @@
 
 Функции:
     - decrypt_command(encrypted_command: str) -> str:
-        Дешифрует строку команд для ровера.
+        Дешифрует инструкцию в последовательность команд для ровера.
     - read_input() -> str:
         Считывает входные данные.
     - print_result(result: str) -> None:
@@ -11,32 +11,35 @@
 """
 
 
-# ID посылки: 123451531
+# ID посылки: 123519155
 def decrypt_command(encrypted_command: str) -> str:
     """
     Дешифруй строку команд для ровера.
 
-    :param encrypted_command: Сжатая строка команд.
+    :param encrypted_command: Шифрованная инструкция.
     :return: Полная строка команд.
     """
     stack: list[tuple[int, str]] = []
-    current_number: int = 0
-    current_string: str = ''
+    current_number: str = ''
+    current_decryption: str = ''
 
     for character in encrypted_command:
-        if character.isdigit():
-            current_number = current_number * 10 + int(character)
+        if '0' <= character <= '9':
+            current_number += character
         elif character == '[':
-            stack.append((current_number, current_string))
-            current_number = 0
-            current_string = ''
+            stack.append((int(current_number), current_decryption))
+            current_number = ''
+            current_decryption = ''
         elif character == ']':
-            previous_number, previous_string = stack.pop()
-            current_string = previous_string + current_string * previous_number
+            previous_number, previous_decryption = stack.pop()
+            current_decryption = (
+                previous_decryption
+                + current_decryption * previous_number
+            )
         else:
-            current_string += character
+            current_decryption += character
 
-    return current_string
+    return current_decryption
 
 
 def read_input() -> str:
@@ -45,8 +48,7 @@ def read_input() -> str:
 
     :return: Строка с шифрованной инструкцией.
     """
-    encrypted_command: str = input()
-    return encrypted_command
+    return input()
 
 
 def print_result(result: str) -> None:
@@ -60,5 +62,4 @@ def print_result(result: str) -> None:
 
 
 if __name__ == '__main__':
-    encrypted_command: str = read_input()
-    print_result(decrypt_command(encrypted_command))
+    print_result(decrypt_command(read_input()))
